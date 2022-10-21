be2645f35aaa9e0650453d34cacbe1bb83c78fe6
Meretskiy Aleksandr <a-meretskiy@mail.ru>
2022-10-22 01:01:03 +0400

    Initial commit

---
+++ src/main/java/com/meretskiy/mytodover/MyTodoVerApplication.java
@@ -0,0 +1,13 @@
+package com.meretskiy.mytodover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoVerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodoVerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meretskiy/mytodover/MyTodoVerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meretskiy.mytodover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoVerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
